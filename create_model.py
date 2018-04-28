3c36f6fe3ab44b3f77fcaf66dd98c4e9085f2dc6
Sean McGlincy <swmcglincy@hotmail.com>
2018-04-27 20:24:20 -0400

    back up lots of features

--- create_model.py
+++ create_model.py
@@ -170,21 +170,21 @@ def common_count(x,y, stopwords=False):
     return count
 
 
-def noun_counter(x,y, stopwords=False):
-    # x = x.split(" ")
-    # y = y.split(" ")
-    # if stopwords:
-    #     x = [PorterStemmer().stem(t) for t in x]
-    #     y = [PorterStemmer().stem(t) for t in y]
-
-    count = 0
-    doc1 = nlp(x)
-    doc2 = nlp(y)
-    for t1 in doc1.noun_chunks:
-        for t2 in doc2.noun_chunks:
-            if t1 == t2:
-                count += 1
-    return count
+# def noun_counter(x,y, stopwords=False):
+#     # x = x.split(" ")
+#     # y = y.split(" ")
+#     # if stopwords:
+#     #     x = [PorterStemmer().stem(t) for t in x]
+#     #     y = [PorterStemmer().stem(t) for t in y]
+#
+#     count = 0
+#     doc1 = nlp(x)
+#     doc2 = nlp(y)
+#     for t1 in doc1.noun_chunks:
+#         for t2 in doc2.noun_chunks:
+#             if t1 == t2:
+#                 count += 1
+#     return count
 
 def cosine_similarity(x,y, w2v=False):
     if x is None or y is None:
@@ -236,9 +236,38 @@ def convert_to_w2v(tokens, w2v):
 
 def convert_to_blob(text):
     opinion = TextBlob(text)
-    return opinion.sentiment
+    return opinion
+    #
+    # noun = opinion.noun_phrases
+    # print(noun)
+    # print(len(noun))
+    # exit(0)
+    # return opinion.sentiment
+
+def sentiment(textblob):
+    return textblob.sentiment
 
 
+def noun_counter(textblob):
+    return len(textblob.noun_phrases)
+def noun_counter_union(textblob_doc, textblob_query):
+    counter =0
+    noun1 = textblob_doc.noun_phrases
+    noun2 = textblob_query.noun_phrases
+    for n1 in noun1:
+        for n2 in noun2:
+            if n1 == n2:
+                counter +=1
+    return counter
+
+def noun_frequency(textblob):
+    return textblob.np_counts
+
+def polarity(textblob):
+    return textblob.polarity
+
+def subjectivity(textblob):
+    return textblob.subjectivity
 # def naiveBayes(text):
 #     opinion = TextBlob(text, analyzer=NaiveBayesAnalyzer())
 #     print(opinion)
@@ -403,16 +432,37 @@ def create_model(all_documents_file, relevance_file,query_file):
     rv["doc_vec_blob"] = rv.apply(lambda x: convert_to_blob(x["title"]), axis=1)
     rv["body_vec_blob"] = rv.apply(lambda x: convert_to_blob(x["body"]), axis=1)
 
-    '''Split tuples'''
-    rv["query_vec_blob_p"],rv["query_vec_blob_s"]=  split_tuples(rv["query_vec_blob"])
-    rv["doc_vec_blob_p"],rv["doc_vec_blob_s"]=  split_tuples(rv["doc_vec_blob"])
-    rv["body_vec_blob_p"],rv["body_vec_blob_s"]=  split_tuples(rv["body_vec_blob"])
 
-    '''Sentament cosine'''
-    rv["cosine_title_pblob"]  = rv.apply(lambda x: cosine_similarity(x['doc_vec_blob_p'], rv["query_vec_blob_p"], w2v=True), axis=1)
-    rv["cosine_title_sblob"]  = rv.apply(lambda x: cosine_similarity(x['doc_vec_blob_s'], rv["query_vec_blob_s"], w2v=True), axis=1)
-    rv["cosine_body_pblob"]  = rv.apply(lambda x: cosine_similarity(x['body_vec_blob_p'], x['query_vec_blob_p'], w2v=True), axis=1)
-    rv["cosine_body_sblob"]  = rv.apply(lambda x: cosine_similarity(x['body_vec_blob_s'], x['query_vec_blob_p'], w2v=True), axis=1)
+    # '''' Textblob noun count '''
+    # rv["query_noun_count"] = rv.apply(lambda x: noun_counter(x["query_vec_blob"]), axis=1)
+    # rv["title_noun_count"] = rv.apply(lambda x: noun_counter(x["doc_vec_blob"]), axis=1)
+    # rv["body_noun_count"] = rv.apply(lambda x: noun_counter(x["body_vec_blob"]), axis=1)
+
+
+    '''' Textblob noun count Union '''
+    rv["title_noun_union"] = rv.apply(lambda x: noun_counter_union(x["doc_vec_blob"], x["query_vec_blob"]), axis=1)
+    rv["body_noun_union"] = rv.apply(lambda x: noun_counter_union(x["body_vec_blob"], x["query_vec_blob"]), axis=1)
+
+
+    '''' polarity '''
+    rv["query_polarity"] = rv.apply(lambda x: polarity(x["query_vec_blob"]), axis=1)
+    rv["title_polarity"] = rv.apply(lambda x: polarity(x["doc_vec_blob"]), axis=1)
+    rv["body_polarity"] = rv.apply(lambda x: polarity(x["body_vec_blob"]), axis=1)
+
+    '''' subjectivity '''
+    rv["query_subjectivity"] = rv.apply(lambda x: subjectivity(x["query_vec_blob"]), axis=1)
+    rv["title_subjectivity"] = rv.apply(lambda x: subjectivity(x["doc_vec_blob"]), axis=1)
+    rv["body_subjectivity"] = rv.apply(lambda x: subjectivity(x["body_vec_blob"]), axis=1)
+
+
+
+    '''cosine portability'''
+    rv["cosine_title_polarity"]  = rv.apply(lambda x: cosine_similarity(x['title_polarity'], rv["query_polarity"], w2v=True), axis=1)
+    rv["cosine_body_polarity"]  = rv.apply(lambda x: cosine_similarity(x['body_polarity'], x['query_polarity'], w2v=True), axis=1)
+
+    ''' cosine subjectivity'''
+    rv["cosine_title_subjectivity"]  = rv.apply(lambda x: cosine_similarity(x['title_subjectivity'], rv["query_subjectivity"], w2v=True), axis=1)
+    rv["cosine_body_subjectivity"]  = rv.apply(lambda x: cosine_similarity(x['body_subjectivity'], x['query_subjectivity'], w2v=True), axis=1)
 
 
 
@@ -445,30 +495,30 @@ def create_model(all_documents_file, relevance_file,query_file):
 
 
 
-    ''' W2V Data '''
-    rv["query_vec_w2v_pos"] = rv.apply(lambda x: np.argmax(x["query_vec_w2v"]), axis=1)
-    rv["query_vec_w2v_max"] = rv.apply(lambda x: np.max(x["query_vec_w2v"]), axis=1)
-    rv["query_vec_w2v_min"] = rv.apply(lambda x: np.min(x["query_vec_w2v"]), axis=1)
-    rv["query_vec_w2v_sum"] = rv.apply(lambda x: np.sum(x["query_vec_w2v"]), axis=1)
-    rv["query_vec_w2v_norm"] = np.divide(rv["query_vec_w2v_sum"] ,rv["len_title_idf"] )
-    rv["query_vec_w2v_prob"] = rv.apply(lambda x: idf_prob(x["query_vec_w2v"]), axis =1)
-
-
-
-    rv["doc_vec_w2v_pos"] = rv.apply(lambda x: np.argmax(x["doc_vec_w2v"]), axis=1)
-    rv["doc_vec_w2v_max"] = rv.apply(lambda x: np.max(x["doc_vec_w2v"]), axis=1)
-    rv["doc_vec_w2v_min"] = rv.apply(lambda x: np.min(x["doc_vec_w2v"]), axis=1)
-    rv["doc_vec_w2v_sum"] = rv.apply(lambda x: np.sum(x["doc_vec_w2v"]), axis=1)
-    rv["doc_vec_w2v_norm"] = np.divide(rv["doc_vec_w2v_sum"] ,rv["len_title_idf"] )
-    rv["doc_vec_w2v_prob"] = rv.apply(lambda x: idf_prob(x["doc_vec_w2v"]), axis =1)
-
-
-    rv["body_vec_w2v_pos"] = rv.apply(lambda x: np.argmax(x["body_vec_w2v"]), axis=1)
-    rv["body_vec_w2v_max"] = rv.apply(lambda x: np.max(x["body_vec_w2v"]), axis=1)
-    rv["body_vec_w2v_min"] = rv.apply(lambda x: np.min(x["body_vec_w2v"]), axis=1)
-    rv["body_vec_w2v_sum"] = rv.apply(lambda x: np.sum(x["body_vec_w2v"]), axis=1)
-    rv["body_vec_w2v_norm"] = np.divide(rv["doc_vec_w2v_sum"] ,rv["len_body_idf"] )
-    rv["body_vec_w2v_prob"] = rv.apply(lambda x: idf_prob(x["body_vec_w2v"]), axis =1)
+    # ''' W2V Data '''
+    # rv["query_vec_w2v_pos"] = rv.apply(lambda x: np.argmax(x["query_vec_w2v"]), axis=1)
+    # rv["query_vec_w2v_max"] = rv.apply(lambda x: np.max(x["query_vec_w2v"]), axis=1)
+    # rv["query_vec_w2v_min"] = rv.apply(lambda x: np.min(x["query_vec_w2v"]), axis=1)
+    # rv["query_vec_w2v_sum"] = rv.apply(lambda x: np.sum(x["query_vec_w2v"]), axis=1)
+    # rv["query_vec_w2v_norm"] = np.divide(rv["query_vec_w2v_sum"] ,rv["len_title_idf"] )
+    # rv["query_vec_w2v_prob"] = rv.apply(lambda x: idf_prob(x["query_vec_w2v"]), axis =1)
+    #
+    #
+    #
+    # rv["doc_vec_w2v_pos"] = rv.apply(lambda x: np.argmax(x["doc_vec_w2v"]), axis=1)
+    # rv["doc_vec_w2v_max"] = rv.apply(lambda x: np.max(x["doc_vec_w2v"]), axis=1)
+    # rv["doc_vec_w2v_min"] = rv.apply(lambda x: np.min(x["doc_vec_w2v"]), axis=1)
+    # rv["doc_vec_w2v_sum"] = rv.apply(lambda x: np.sum(x["doc_vec_w2v"]), axis=1)
+    # rv["doc_vec_w2v_norm"] = np.divide(rv["doc_vec_w2v_sum"] ,rv["len_title_idf"] )
+    # rv["doc_vec_w2v_prob"] = rv.apply(lambda x: idf_prob(x["doc_vec_w2v"]), axis =1)
+    #
+    #
+    # rv["body_vec_w2v_pos"] = rv.apply(lambda x: np.argmax(x["body_vec_w2v"]), axis=1)
+    # rv["body_vec_w2v_max"] = rv.apply(lambda x: np.max(x["body_vec_w2v"]), axis=1)
+    # rv["body_vec_w2v_min"] = rv.apply(lambda x: np.min(x["body_vec_w2v"]), axis=1)
+    # rv["body_vec_w2v_sum"] = rv.apply(lambda x: np.sum(x["body_vec_w2v"]), axis=1)
+    # rv["body_vec_w2v_norm"] = np.divide(rv["doc_vec_w2v_sum"] ,rv["len_body_idf"] )
+    # rv["body_vec_w2v_prob"] = rv.apply(lambda x: idf_prob(x["body_vec_w2v"]), axis =1)
 
 
 
@@ -491,74 +541,60 @@ def create_model(all_documents_file, relevance_file,query_file):
 
 
     ''' Step 6. Defining the feature and label  for classification'''
-
-    X = rv[["avg_cos_title"] + ["cosine_title"] + ["cosine_title_w2v"] + ["common_title"]  + ["common_title_s"] + ["avg_cos_body"] + [
+    X = rv[["cosine_title"] + ["cosine_title_w2v"] + ["common_title"]  + ["common_title_s"] + [
         "cosine_body"] + ["cosine_body_w2v"] + ["common_body"]+ ["common_body_s"]
 
 
-           + ["cosine_title_pblob"] + ["cosine_title_sblob"] + ["cosine_body_pblob"] + ["cosine_body_sblob"]
+           + ["cosine_title_polarity"] + ["cosine_title_subjectivity"] + ["cosine_body_polarity"] + ["cosine_body_subjectivity"]
            + ["max_query_idf"] + ["max_pos_query_idf"] + ["norm_query_idf"] + ["len_query_idf"] + ["prob_query_idf"]+["mean_query_idf"]
            + ["max_title_idf"] + ["max_pos_title_idf"] + ["norm_title_idf"] + ["len_title_idf"] + ["prob_title_idf"]+["mean_title_idf"]
            + ["max_body_idf"] + ["max_pos_body_idf"] + ["norm_body_idf"] + ["len_body_idf"] + ["prob_body_idf"]+["mean_body_idf"]
-           + ["query_vec_blob_p"] + ["query_vec_blob_s"]
-           + ['doc_vec_blob_p'] + ['doc_vec_blob_s']
-           + ['body_vec_blob_p'] + ['body_vec_blob_s']
-
-           # + ["query_title_max"] + ["query_body_max"]
-           # + ["query_title_norm"] + ["query_body_norm"]
-           # + ["query_title_prob"]+ ["query_body_prob"]
-           #  +  ["query_title_sum"] +["query_body_sum"]
-           # + ["query_title_max_pos"] + ["query_body_max_pos"]
-
-
+           + ["title_noun_union"] + ["body_noun_union"]
     ]
 
-    # + ["query_title_max"] + ["query_title_norm"] + ["query_title_sum"]
-    # #     + ["query_body_max_pos"] + ["query_body_max"]
 
 
-    # X = rv[["avg_cos_title"] + ["cosine_title"] + ["cosine_title_w2v"] + ["avg_cos_body"] + [
-    #     "cosine_body"] + ["cosine_body_w2v"]
-
-           #
-           # + ["cosine_title_pblob"] + ["cosine_title_sblob"] + ["cosine_body_pblob"] + ["cosine_body_sblob"]
-           # + ["max_query_idf"] + ["max_pos_query_idf"] + ["norm_query_idf"] + ["len_query_idf"] + ["prob_query_idf"]+["mean_query_idf"]
-           # + ["max_title_idf"] + ["max_pos_title_idf"] + ["norm_title_idf"] + ["len_title_idf"] + ["prob_title_idf"]+["mean_title_idf"]
-           # + ["max_body_idf"] + ["max_pos_body_idf"] + ["norm_body_idf"] + ["len_body_idf"] + ["prob_body_idf"]+["mean_body_idf"]
-           # + ["query_vec_blob_p"] + ["query_vec_blob_s"]
-           # + ['doc_vec_blob_p'] + ['doc_vec_blob_s']
-           # + ['body_vec_blob_p'] + ['body_vec_blob_s']
 
 
+
+    # X = rv[["cosine_title"] + ["cosine_title_w2v"] + ["common_title"]  + ["common_title_s"] + [
+    #     "cosine_body"] + ["cosine_body_w2v"] + ["common_body"]+ ["common_body_s"]
+    #
+    #
+    #        # + ["cosine_title_polarity"] + ["cosine_title_subjectivity"] + ["cosine_body_polarity"] + ["cosine_body_subjectivity"]
+    #        + ["max_query_idf"] + ["max_pos_query_idf"] + ["norm_query_idf"] + ["len_query_idf"] + ["prob_query_idf"]+["mean_query_idf"]
+    #        + ["max_title_idf"] + ["max_pos_title_idf"] + ["norm_title_idf"] + ["len_title_idf"] + ["prob_title_idf"]+["mean_title_idf"]
+    #        + ["max_body_idf"] + ["max_pos_body_idf"] + ["norm_body_idf"] + ["len_body_idf"] + ["prob_body_idf"]+["mean_body_idf"]
+    #        # + ["query_vec_blob_p"] + ["query_vec_blob_s"]
+    #        # + ['doc_vec_blob_p'] + ['doc_vec_blob_s']
+    #        # + ['body_vec_blob_p'] + ['body_vec_blob_s']
+    #        # + ["query_polarity"] + ["title_polarity"] + ["body_polarity"]
+    #        # + ["query_subjectivity"] + ["title_subjectivity"] + ["body_subjectivity"]
+    #        # +["query_noun_count"] + ["title_noun_count"] + ["body_noun_count"]
+    #        + ["title_noun_union"] + ["body_noun_union"]
+    # ]
+    #
+
+
+
+
+    # X = rv[["avg_cos_title"] + ["cosine_title"] + ["cosine_title_w2v"] + ["common_title"]  + ["common_title_s"] + ["avg_cos_body"] + [
+    #     "cosine_body"] + ["cosine_body_w2v"] + ["common_body"]+ ["common_body_s"]
+    #
+    #
+    #        # + ["cosine_title_polarity"] + ["cosine_title_subjectivity"] + ["cosine_body_polarity"] + ["cosine_body_subjectivity"]
+    #        + ["max_query_idf"] + ["max_pos_query_idf"] + ["norm_query_idf"] + ["len_query_idf"] + ["prob_query_idf"]+["mean_query_idf"]
+    #        + ["max_title_idf"] + ["max_pos_title_idf"] + ["norm_title_idf"] + ["len_title_idf"] + ["prob_title_idf"]+["mean_title_idf"]
+    #        + ["max_body_idf"] + ["max_pos_body_idf"] + ["norm_body_idf"] + ["len_body_idf"] + ["prob_body_idf"]+["mean_body_idf"]
+    #        # + ["query_vec_blob_p"] + ["query_vec_blob_s"]
+    #        # + ['doc_vec_blob_p'] + ['doc_vec_blob_s']
+    #        # + ['body_vec_blob_p'] + ['body_vec_blob_s']
+    #        # + ["query_polarity"] + ["title_polarity"] + ["body_polarity"]
+    #        # + ["query_subjectivity"] + ["title_subjectivity"] + ["body_subjectivity"]
+    #        # +["query_noun_count"] + ["title_noun_count"] + ["body_noun_count"]
+    #        + ["title_noun_union"] + ["body_noun_union"]
     # ]
 
-
-    #
-    # X = rv[
-    #     ["cosine_title"]+ ["cosine_title_w2v"]+["common_title"] + ["cosine_body"] + ["cosine_body_w2v"] + ["common_body"]
-    #     + ["cosine_title_pblob"] + ["cosine_title_sblob"] + ["cosine_body_pblob"] + ["cosine_body_sblob"]
-    #     # ["common_title"] + ["common_body"]
-    #     + ["common_title_s"] + ["common_body_s"]
-    #     + ["max_query_idf"] + ["norm_query_idf"] + ["sum_query_idf"] + ["len_query_idf"] + ["prob_query_idf"] +["mean_query_idf"]
-    #     + ["max_title_idf"] + ["norm_title_idf"] + ["sum_title_idf"] + ["len_title_idf"] + ["prob_title_idf"]+["mean_title_idf"]
-    #     + ["max_body_idf"] + ["norm_body_idf"] + ["sum_body_idf"] + ["len_body_idf"] + ["prob_body_idf"] +["mean_body_idf"]
-    #     + ["query_vec_w2v_sum"]+ ["doc_vec_w2v_sum"] + ["body_vec_w2v_sum"]
-    #     + ["query_vec_w2v_max"]+ ["doc_vec_w2v_max"] + ["body_vec_w2v_max"]
-    #
-    #     + ["query_vec_w2v_max"] + ["query_vec_w2v_norm"] + ["query_vec_w2v_sum"] + ["query_vec_w2v_prob"]
-    #     + ["doc_vec_w2v_max"] + ["doc_vec_w2v_norm"] + ["doc_vec_w2v_sum"] + ["doc_vec_w2v_prob"]
-    #     + ["body_vec_w2v_max"] + ["body_vec_w2v_norm"] + ["body_vec_w2v_sum"] + ["body_vec_w2v_prob"]
-    #     + ["query_title_max_pos"] + ["query_title_max"] + ["query_title_norm"] + ["query_title_sum"]
-    #     + ["query_body_max_pos"] + ["query_body_max"] + ["query_body_norm"] + ["query_body_sum"]
-    #     + ["title_count"] + ["body_count"]
-    #     + ["query_vec_blob_p"] + ["query_vec_blob_s"]
-    #     + ['doc_vec_blob_p'] + ['doc_vec_blob_s']
-    #     + ['body_vec_blob_p'] + ['body_vec_blob_s']
-    #     # + ["common_title_noun"] + ["common_body_noun"]
-    #     # + ["title_spacy"] + ["body_spacy"]
-    #
-    #     ]
-    # #
 
     Y = [v for k, v in rv["position"].items()]
     # from sklearn import preprocessing
